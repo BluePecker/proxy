14b7459ded034809be6b7986ba79b31c56f2dd27
shuchao <645904877@qq.com>
2017-08-09 11:48:24 +0800

    fix bug

--- app.js
+++ app.js
@@ -13,25 +13,11 @@ import Request from 'request';
 const app = new Koa();
 const router = new Router();
 
-router.get('/luis/query', ctx => {
-    Request({
-        uri: `https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/61ef41a0-b9c4-483c-b787-c13bacd90e13`,
-        qs: ctx.query,
-        headers: ctx.header,
-        json: true
-    }).then(res => {
-        ctx.body = res;
-    }).catch(() => {
-        ctx.body = {};
-    });
-});
-
 router.get('/luis/:version/apps/:app_id', (ctx) => {
     /**
      * @typedef {{params:{app_id:string}}} ctx
      * @typedef {{then:function}} Request
      */
-    console.log(`https://westus.api.cognitive.microsoft.com/luis/${ctx.params.version}/apps/${ctx.params.app_id}`);
     ctx.body = Request.get({
         url: `https://westus.api.cognitive.microsoft.com/luis/${ctx.params.version}/apps/${ctx.params.app_id}`,
         qs: ctx.query,
